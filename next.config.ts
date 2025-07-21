fcddb03f1aa5d6b10dd9f7db5105af7b10811fa2
Radmir <Radmir050403@mail.ru>
2025-07-21 12:03:10 +0300

    ошибка 123

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const withNextIntl = createNextIntlPlugin();
 
+/** @type {import('NextConfig')} */
 const nextConfig: NextConfig = {
   turbopack: {} 
 };
 
-export default createNextIntlPlugin()(nextConfig)
+// export default createNextIntlPlugin()(nextConfig)
+
+
+
+
+export default withNextIntl(nextConfig);
